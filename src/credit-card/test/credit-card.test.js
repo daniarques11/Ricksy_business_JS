c19a2109f819e70089664b7eaf9af4fd083a0249
darques <darques@arteco-consulting.com>
2020-12-07 13:53:07 +0100

    test: Añadir casos test de prototipo a creditCard

--- src/credit-card/test/credit-card.test.js
+++ src/credit-card/test/credit-card.test.js
@@ -9,16 +9,41 @@ test("Credit card is instanciated correctly", () => {
     expect(person.credit).toBe(3000);
 })
 
-test("Instances are not same object", () => {
-    let james = instanceCreditCard("James", 111);
-    let john = instanceCreditCard("John", 112);
-    let jamesImpersonator = instanceCreditCard("James", 111);
-    expect(james).not.toBe(john);
-    expect(james).not.toBe(jamesImpersonator);
-    expect(Object.getPrototypeOf(james)).toBe(Object.getPrototypeOf(jamesImpersonator));
+describe("Instances checks", () => {
+    let james;
+    let john;
+    let jamesImpersonator;
+    beforeEach(() => {
+        james = instanceCreditCard("James", 111);
+        john = instanceCreditCard("John", 112);
+        jamesImpersonator = instanceCreditCard("James", 111);
+    })
+    test("Different instances are not same object", () => {
+        expect(james).not.toBe(john);
+        expect(james).not.toBe(jamesImpersonator);
+    })
+    test("Different instances have same prototype", () => {
+        expect(Object.getPrototypeOf(james)).toBe(Object.getPrototypeOf(jamesImpersonator));
+    })
+
+    test("Property added to one instance isn't shared", () => {
+        james.authenticity = true;
+        expect(james.authenticity).toBeTruthy();
+        expect(jamesImpersonator.authenticity).toBeUndefined()
+    })
+
+    test("Property added to the prototype is shared", () => {
+        //Add new method to the prototype
+        Object.getPrototypeOf(james).getSalute = function() {
+            return `Hi, I'm ${this.name}`;
+        }
+        expect(james.getSalute()).toBe("Hi, I'm James");
+        expect(jamesImpersonator.getSalute()).toBe("Hi, I'm James");
+    })
 })
 
-describe("Paying method tests", () => {
+
+describe("Paying method ", () => {
     let payer;
 
     //Reinstance before each test to reset the credit to 3000
@@ -50,5 +75,4 @@ describe("Paying method tests", () => {
             expect(payer.credit).toBe(0);
         })
     })
-
 });
